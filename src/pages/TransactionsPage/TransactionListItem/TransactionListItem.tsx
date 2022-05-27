4f0958499b5063443bba261f2c282ed1549d32ea
Tony Wu <tw.wu@yahoo.com>
2022-05-26 23:58:49 -0400

    complete transactionPage

--- src/pages/TransactionsPage/TransactionListItem/TransactionListItem.tsx
+++ src/pages/TransactionsPage/TransactionListItem/TransactionListItem.tsx
@@ -1,15 +1,39 @@
-import { Text } from '@mantine/core';
+import { Box, Divider, Text } from '@mantine/core';
 import React from 'react';
-import type { Transaction } from '../TransactionsPage.component';
 
 type TransactionListItemProps = Transaction;
 
+type TransactionType = 'deposit' | 'interest' | 'withdrawal' | 'moonSave';
+
+export interface Transaction {
+  transactionId: number;
+  transactionType: TransactionType;
+  transactionAmount: number;
+  transactionDate: Date;
+  totalBankAmount: number;
+}
+
+const transactionTypeToName: Record<TransactionType, string> = {
+  deposit: "Deposit",
+  interest: "Interest Payout",
+  withdrawal: "Withdrawal",
+  moonSave: "MoonSave Payout🎉"
+}
+
 export const TransactionListItem: React.FC<TransactionListItemProps> = ({ transactionType, transactionAmount, transactionDate, totalBankAmount }) => (
-  <>
-    <Text>
-      {/* TODO: Date format string */}
-      {transactionType} {transactionAmount} {totalBankAmount}
-    </Text>
-  </>
+
+  <><Box sx={{width: '100%', justifyContent: 'space-between', display: 'flex', paddingBlock: '0.5rem'}}>
+        <Box sx={{display: 'flex', flexDirection: 'column'}}>
+          <Text align='left' weight='bold'>{transactionTypeToName[transactionType]}</Text>
+          <Text size='xs' color='gray'>{transactionDate.toDateString()}</Text>
+        </Box>
+        <Box sx={{display: 'flex', flexDirection: 'column', alignItems: 'flex-end'}}>
+          <Text align='right' weight='bold' color={transactionType==='withdrawal'? 'red':'green'}>
+            {transactionType==='withdrawal'? '-':'+'}{transactionAmount} ETH
+          </Text>
+          <Text size='xs' color='gray'>${totalBankAmount} USD</Text>
+        </Box>
+      </Box>
+      <Divider/></>
 );
 TransactionListItem.displayName = 'TransactionListItem';
--- src/pages/TransactionsPage/TransactionsPage.component.tsx
+++ src/pages/TransactionsPage/TransactionsPage.component.tsx
@@ -1,42 +1,25 @@
 import React from 'react';
-import { Box, NumberInputHandlers, ScrollArea, Text } from '@mantine/core';
-import { TransactionListItem } from './TransactionListItem/TransactionListItem';
+import { Box, Divider, NumberInputHandlers, ScrollArea, Space, Text } from '@mantine/core';
+import { Transaction, TransactionListItem } from './TransactionListItem/TransactionListItem';
 import { BalanceDisplay } from '../../components/BalanceDisplay/BalanceDisplay';
 
-export interface Transaction {
-  transactionId: number;
-  transactionType: 'deposit' | 'interest' | 'withdrawal' | 'MoonSave';
-  transactionAmount: number;
-  transactionDate: Date;
-  totalBankAmount: number;
-}
 interface TransactionsPageProps {
   transactionsList: Transaction[];
 }
 
 export const TransactionsPage: React.FC<TransactionsPageProps> = ({ transactionsList }) => {
   return (
-  //   <Box sx={{ width: '100%', display: 'grid', gridTemplateColumns: '1fr 1fr' }}>
-  //   <Box sx={{ display: 'flex', flexDirection: 'column', gap: '4rem', justifyContent: 'center' }}>
-  //     <Text sx={{ fontSize: '4rem' }}>History</Text>
-  //     {/* <BalanceDisplayContainer /> */}
-  //     {/* <Box sx={{ display: 'flex', gap: '1rem' }}>
-  //       <Button>Deposit</Button>
-  //       <Button>Withdraw</Button>
-  //     </Box> */}
-  //   </Box>
-  // </Box>
-  // <Box>
-  //   <Text>Graph here</Text>
-  // </Box>
-  <Box>
-    <Text>HISTORY</Text>
-    <BalanceDisplay myETHBalance={3.3722} balanceDelta={523.23} myOddsToWin={0.6712} showOddsToWin={false} />
-    <ScrollArea>
-      {transactionsList.map((transaction) => (
-        <TransactionListItem key={transaction.transactionId} {...transaction} />
-      ))}
-    </ScrollArea>
+  <Box sx={{ width: '100%', display: 'flex', flexDirection: 'column', gridTemplateColumns: '1fr 1fr'}}>
+    <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'flex-start' }}>
+      <Text sx={{ fontSize: '4rem' }}>History</Text>
+      <BalanceDisplay myETHBalance={3.3722} balanceDelta={523.23} myOddsToWin={0.6712} showOddsToWin={false} />
+    </Box>
+    <Text><br></br></Text>
+      <ScrollArea sx={{width: '100%', gap: '4rem'}}>
+        {transactionsList.map((transaction) => (
+          <TransactionListItem key={transaction.transactionId} {...transaction} />
+        ))}
+      </ScrollArea>
   </Box>
   );
 };
